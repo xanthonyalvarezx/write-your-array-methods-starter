ad06e3915f70532b18c3d495f1b4601cacbe4cde
tamburkey <petem14@gmail.com>
2020-06-27 00:04:21 -0400

    refactor function syntax

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let testNumsArray = [3, 1, -5, 2, 105, -24, 495, 8, 246]
 let testWordsArray = ['potato', 'rainbow', 'cloud', 'duck', 'orange', 'three']
 
 // This is one example of implementing the functionality of forEach:
-const newForEach = function(array, callback){
+function newForEach(array, callback){
     for (let index = 0; index < array.length; index = index + 1){
         let currentItem = array[index]
         callback(currentItem, index, array)
