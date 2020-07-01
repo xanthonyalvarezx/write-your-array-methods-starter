50fa9ea103f403102e060cf288ed70328ae2a550
AntMan <xanthonyalvarezx@gmail.com>
2020-06-30 21:11:11 -0400

    initial commit

--- main.js
+++ main.js
@@ -5,14 +5,16 @@ let testNumsArray = [3, 1, -5, 2, 105, -24, 495, 8, 246]
 let testWordsArray = ['potato', 'rainbow', 'cloud', 'duck', 'orange', 'three']
 
 // This is one example of implementing the functionality of forEach:
+console.groupCollapsed('forEach')
 function newForEach(array, callback){
     for (let index = 0; index < array.length; index = index + 1){
         let currentItem = array[index]
         callback(currentItem)
     }
 }
-
+console.groupEnd()
  
+console.groupCollapsed('map')
 function newMap(array, callback){
    
         let map1 = []
@@ -23,51 +25,59 @@ function newMap(array, callback){
     }
     return map1
 }
- 
+ console.groupEnd()
 
-
+ console.groupCollapsed('some')
 function newSome(array, callback){
     for (let index = 0; index < array.length; index = index + 1){
         let currentItem = array[index]
        
         callback(currentItem,index,array)
-       return  array.some(callback)
         
     }
+    return  array.some(callback)
 }
+console.groupEnd()
 
+console.groupCollapsed('find')
 function newFind(array, callback){
     for (let index = 0; index < array.length; index = index + 1){
         let currentItem = array[index]
         callback(currentItem,index,array)
-       return array.find(callback)
     }
+    return array.find(callback)
 }
+console.groupEnd()
 function newFindIndex(array, callback){
     for (let index = 0; index < array.length; index = index + 1){
         let currentItem = array[index]
         callback(currentItem,index,array)
-       return array.findIndex(callback)
     }
+    return array.findIndex(callback)
 }
+console.groupCollapsed('every')
 function newEvery(array, callback){
     for (let index = 0; index < array.length; index = index + 1){
         let currentItem = array[index]
         callback(currentItem,index,array)
-       return array.every(callback)
     }
+    return array.every(callback)
 }
 function newFilter(array, callback){
     for (let index = 0; index < array.length; index = index + 1){
         let currentItem = array[index]
         callback(currentItem,index,array)
-       return array.every(callback)
     }
+    return array.every(callback)
 }
+console.groupEnd()
+
+console.groupCollapsed('filter')
 function newFilter(array, callback){
     for (let index = 0; index < array.length; index = index + 1){
         let currentItem = array[index]
         callback(currentItem,index,array)
-       return array.filter(callback)
     }
+    return array.filter(callback)
 }
+console.groupEnd()
